ef105509815ebde61fb86e9d2635d4311164b952
shandowpro <115099169+shandowpro@users.noreply.github.com>
2024-07-13 21:48:25 +0300

    Add files via upload

---
+++ refs/20 TypeScript/2 Install TypeScript And Transpile Files/index.ts
@@ -0,0 +1,12 @@
+
+// This is the first TS file for test running the JS code inside it :  
+
+
+  ()  =>  {
+    // The following function need a parameter => [this shall display {Static type Checking}  ]: 
+    // console.log(Math.round( ) );
+    
+    // This is the proper syntax :
+    console.log(Math.round(2.5) );
+
+}
