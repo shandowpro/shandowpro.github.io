d48c6f932c3014e9b03f586ff5dc3ef037ca2e1e
shandowpro <115099169+shandowpro@users.noreply.github.com>
2024-07-14 16:56:26 +0300

    Add files via upload

---
+++ refs/20 TypeScript/4 Statically vs Dynamically Typed Languages/src/index.ts
@@ -0,0 +1,9 @@
+// [TS] lesson =>   3 Create Configuration And Watch Files :
+
+// This is the first TS file for test running the JS code inside it :  
+
+// The following function need a parameter => [this shall display {Static type Checking}  ]: 
+console.log(Math.round(10.5 ) );
+
+// This is the proper syntax :
+console.log(Math.round(2.5) );
