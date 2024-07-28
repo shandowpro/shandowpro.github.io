4658d2ae28738fb39cd2e504d5c98a7bdba8847f
shandowpro <115099169+shandowpro@users.noreply.github.com>
2024-07-28 19:33:26 +0300

    Add files via upload

---
+++ refs/16 JQuery Course/02 jQuery - Syntax/js/myplugins.js
@@ -0,0 +1,40 @@
+
+// Main lessons functions : 
+
+// 1) Changing the color of the [parageraph] when the page is being loaded: 
+$(document).ready(function () {
+    $('p').css("color", "green");
+
+});
+
+// 2) Hiding the [parageraph] when the page is being loaded: 
+$(document).ready(function () {
+    $('p').hide();
+
+});
+
+
+// Activating the JQuery file function in the page [click on the button] :
+// 1) Changing the color of the [parageraph] when the page is being loaded: 
+function testj1() {
+    // $(document).ready(function () {
+    $('.display').css("color", "green");
+
+    // });
+}
+
+function testj2() {
+    // 2) Hiding the [parageraph] when the page is being loaded: 
+    // $(document).ready(function () {
+    $('.display').hide();
+
+    // });
+
+}
+
+// -------------------------------------------------------
+
+// function display() {
+
+// }
+
