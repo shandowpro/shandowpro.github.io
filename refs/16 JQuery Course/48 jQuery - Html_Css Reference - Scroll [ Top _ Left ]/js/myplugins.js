56efa09e283b5d4035def6876433da1281416dd8
shandowpro <115099169+shandowpro@users.noreply.github.com>
2024-07-31 15:25:13 +0300

    Add files via upload

---
+++ refs/16 JQuery Course/48 jQuery - Html_Css Reference - Scroll [ Top _ Left ]/js/myplugins.js
@@ -0,0 +1,171 @@
+//  48 jQuery - Html_Css Reference - Scroll [ Top _ Left ]
+/* Main lessons functions :
+    a] Main html/css References {part 8} ->  {scrollTop()} {scrollLeft()}  =>   [ 2 ] methods :
+                                
+        1) First function [Using {scrollTop()} ] {2 functions} -> [ Apply certain effect by reaching (window scroll top) to an (element offset top)  ]          
+        2) Second function [Using {scrollTop()} ] {4 functions} -> [ Navigate to a certain position location [top/bottom of window] [effset top of certain element ] with [Insantantly , Smoothy] in the web document -> by clicking on a certain button ] 
+                 
+    b] Main used methods within selector used in this lessons functions : 
+      (1) {scroll()} : Event for the window  
+      (2) {scroll(#value)} : Set the  window scroll top with a certain value
+      (3) {scroll(elem.offset().top)} : Set the  window scroll top with a certain element offset top value
+      (4) {offset().top} : to use a cetain element offset top in the window      
+      (5) using Event {click()} to perform the action 
+      (6) Effects {fadeIn() & fadeOut()} 
+      (7) animate({}) method
+
+*/
+// ========================================================
+// I] Orcedion Plus buttons effect [5] groups :
+// 1) First plus button :
+$(document).ready(function () {
+    $('#plus1').click(function () {
+        $('#group1').slideToggle('slow');
+    });
+});
+
+// 2) Second plus button :
+$(document).ready(function () {
+    $('#plus2').click(function () {
+        $('#group2').slideToggle('slow');
+    });
+});
+
+// 3) Third plus button :
+$(document).ready(function () {
+    $('#plus3').click(function () {
+        $('#group3').slideToggle('slow');
+    });
+});
+
+// 4) Fourth plus button :
+$(document).ready(function () {
+    $('#plus4').click(function () {
+        $('#group4').slideToggle('slow');
+    });
+});
+
+// 5) Fifth plus button :
+$(document).ready(function () {
+    $('#plus5').click(function () {
+        $('#group5').slideToggle('slow');
+    });
+});
+
+// 6) Sixth plus button :
+$(document).ready(function () {
+    $('#plus6').click(function () {
+        $('#group6').slideToggle('slow');
+    });
+});
+
+// 7) Seventh plus button :
+$(document).ready(function () {
+    $('#plus7').click(function () {
+        $('#group7').slideToggle('slow');
+    });
+});
+// =========================================================
+// =========================================================
+// =========================================================
+
+// II] Functions variables [2] main Groups functions :
+
+// $(document).ready(function () {
+$(function () {
+    'use strict';
+
+    // I] Definition of all elemensts :
+    alert('Hello to the JQ [scrollTop()] Method ');
+
+    var elem1 = $(".codes0 #elem1"),
+        btn1 = $(".codes0 #btnTop1"),
+        btn2 = $(".codes0 #btnTop2"),
+        btn3 = $(".codes0 #btnTop3"),
+        btn4 = $(".codes0 #btnTop4");
+
+
+
+    // II] Main functions Groups :
+
+    // 1) First function [Using {scrollTop()} ] {2 functions} -> [ Apply certain effect by reaching (window scroll top) to an (element offset top)  ]  ->
+    $(window).scroll(function () {
+
+        // 1) printing the [window scroll] top value in the console :
+        console.log('window scroll top = ' + $(this).scrollTop());
+
+        // 2) printing the cetain element scroll Top value in the console :
+        console.log('Elem1 offset top  = ' + elem1.offset().top);
+
+        if ($(window).scrollTop() >= elem1.offset().top) {
+            elem1.animate({
+                opacity: 1
+            }, 500);
+        }
+    });
+
+    // ------------------------------------------------
+
+    // 2) Second function [Using {scrollTop()} ] {2 functions} -> [ Navigate to a certain position location in the web document -> by clicking on a certain button ]Second function [Using {scrollTop()} ] {2 functions} -> [ Navigate to a certain position location in the web document -> by clicking on a certain button ]->
+
+    // 1) Navigate to Top of window screen [Instantly Mode] :
+    btn1.click(function () {
+        $(window).scrollTop(0);
+    });
+
+    // 2) Navigate to Top of window screen [Smooth Mode] :
+    btn2.click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
+    // 3) Navigate to Top of certain element location [Instantly Mode] :
+    btn3.click(function () {
+        $(window).scrollTop(elem1.offset().top);
+    });
+
+
+    // 4) Navigate to Top of certain element location [Smooth Mode] :
+    btn4.click(function () {
+        $('html,body').animate({
+            scrollTop: elem1.offset().top
+        }, 1000);
+    });
+
+});
+
+// ------------------------------------------------
+// ------------------------------------------------------
+
+
+// Fixed functions:
+// Scroll to top/bottom buttons :
+
+$(function () {
+    'use strict';
+
+    $('.Home').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000).delay(500);
+    })
+
+    $('.End').click(function () {
+        $('html,body').animate({
+            scrollTop: $('#footer').offset().top
+        }, 1000).delay(500);
+    })
+
+
+})
+
+
+
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+
